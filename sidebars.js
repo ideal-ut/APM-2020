f9c5ac963d5d7d6b35f832d96b98432c2f1ed710
Somnath Rakshit <12930218+somnathrakshit@users.noreply.github.com>
2020-09-16 06:18:01 -0500

    WIP

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,14 @@
 module.exports = {
-  seca: ["sec-a/a-1-gaussian-dist",
-    "sec-a/a-2-doc",
-  ],
-  secb: ["sec-b/b-1-gaussian-dist",
-    "sec-b/b-2-doc",
+  docs: [
+    {
+      type: 'category',
+      label: 'Section A',
+      items: ['sec-a/a-1-gaussian-dist'],
+    },
+    {
+      type: 'category',
+      label: 'Section B',
+      items: ['sec-b/b-1-gaussian-dist'],
+    },
   ],
 };
